5d86ec2c447ce39113fb0ff25f2a43dab85e1753
Pravin Dhas <pravinsdhas@gmail.com>
2020-05-10 20:56:49 +0530

    one more commit

--- src/test/java/WeatherTest.java
+++ src/test/java/WeatherTest.java
@@ -7,7 +7,7 @@ public class WeatherTest {
 	public static void main(String args[]) {
 		System.out.println("a");
 		Response resp=when().get("api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=2c6fa043ae82443537201cb41f09e101");
-		System.out.println("Response Code: "+resp.statusCode());
+		System.out.println("Response Code  : "+resp.statusCode());
 	}
 
 }
